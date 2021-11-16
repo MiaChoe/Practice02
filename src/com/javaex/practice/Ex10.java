534f1954f0cb1262441ec87d4d1caa49da39a434
최승은 <liberalfilm@gmail.com>
2021-11-16 16:19:04 +0900

    2021.11.16(화)16:18

--- src/com/javaex/practice/Ex10.java
+++ src/com/javaex/practice/Ex10.java
@@ -1,4 +1,5 @@
-package com.javaex.practice;//출제의도:나머지3인 걸 따로 얘기 안 했으니 default를 A팀으로 둔다
+package com.javaex.practice;//Done//출제의도:나머지3인 걸 따로 얘기 안 했으니 default를 A팀으로 둔다+음수를 마지막에 두니 안 되네
+							//0번을 A팀에 넣기 위해서 ||연산자 넣음
 
 import java.util.Scanner;
 
@@ -9,14 +10,35 @@ public class Ex10 {
 
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("사번(정수)을 입력해주세요");
+		System.out.print("사번: ");
+		
+		int num = sc.nextInt();
+		
+		
+			if (num<0){
+				System.out.println("잘못입력하셨습니다.");
+				}
+				
+			else if (num%3==2) {
+				System.out.println("C팀입니다.");
+				}
+			
+			else if (num%3==0 || num==0) {
+				System.out.println("A팀입니다.");
+				}
+			
+			else if (num%3==1) {
+				System.out.println("B팀입니다.");
+				}
+			
+			
+			sc.close();
+		
+		}
+		
+}	
 		
 		
 		
 		
-		
-		sc.close();
-		
-		
-	}
-
-}
--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -1,10 +1,43 @@
-package com.javaex.practice;
+package com.javaex.practice;//Yet Done //출제의도:switch-case, 오랜만에 nextInt대신 nextLine, case뒤에 숫자 아닌 문자
+
+import java.util.Scanner;
 
 public class Ex11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("알파벗 1글자(소문자)를 입력하세요");
+		System.out.print("알파벳:");
+		
+		String alphabet = sc.nextLine();
+		
+		switch (alphabet) {
+			case a
+			case e
+			case i
+			case o
+			case u
+				System.out.println("모음입니다");
+		break;
+			}
+		
+			default : {
+				System.out.println("자음입니다");
+			}
+		}
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+		
+		
 	}
 
 }
