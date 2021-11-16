7a6f84a43d318e385546eb23d3f2298311a79783
최승은 <liberalfilm@gmail.com>
2021-11-16 16:27:23 +0900

    2021.11.16(화)16:27

--- src/com/javaex/practice/Ex12.java
+++ src/com/javaex/practice/Ex12.java
@@ -1,10 +1,31 @@
-package com.javaex.practice;
+package com.javaex.practice;//출제의도:.0이니까 int아니고 double
+
+import java.util.Scanner;
 
 public class Ex12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("출력되는 내용을 입력하세요");
+		System.out.print("기호:");
+		String arithmetic = sc.nextLine();
+		
+		System.out.print("숫자1");
+		double num1 = sc.nextDouble();
+		
+		System.out.println("숫자2:");
+		double num2 = sc.nextDouble();
+		
+		if (num1)
+		
+		
+		
+		
+		sc.close();
+		
 	}
 
 }
--- src/com/javaex/practice/Ex13.java
+++ src/com/javaex/practice/Ex13.java
@@ -1,10 +1,21 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex13 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+			
+			
+			
+			
+			
+		sc.close();
+		
+		
 	}
 
 }
