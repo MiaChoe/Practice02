d8491e3ae80bca147af40ce991466b9fb17653fc
최승은 <liberalfilm@gmail.com>
2021-11-16 17:55:46 +0900

    2021.11.16(화)17:55

--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -1,8 +1,12 @@
 package com.javaex.practice;//Yet Done //출제의도:switch-case, 오랜만에 nextInt대신 nextLine, case뒤에 숫자 아닌 문자
 										//string아니야, 한 글자니까 char야!=>그런데 불러들이는 next뒤가 Char가 아닌가봐;
 										//=>질문 후: char의 직접식별자는 없었고 객체지향 가면 배운다는 char(0) sort of
+										//수업 중,기본자료형 8개는 if(num==3)이렇게 같다는 표현이 되는데 
+										//String str = "안녕"; if(str.equals("안녕")) String은 안되고 앞으로 기본자료형8개 뺀 나머진 
+	
 
 import java.util.Scanner;
+
 
 public class Ex11 {
 
